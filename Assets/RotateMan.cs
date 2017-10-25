099f5a8a5a2a13eda21f29472a2a36c2b6423e13
tendopain55 <wht5500@gmail.com>
2017-10-24 17:57:03 -0700

    1st
    1st

---
+++ Assets/RotateMan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody snowman = this.GetComponent<Rigidbody>();
+        snowman.AddRelativeTorque(0, 45, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // this.transform.Rotate(0, 4, 0);
+        
+	}
+}
