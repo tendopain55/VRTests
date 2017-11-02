4f6ae1553a13954bd35d2c90bfbce15624ae9835
Unknown <wht5500@gmail.com>
2017-11-02 16:40:26 -0700

    Circle tied to object

---
+++ Assets/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vectrosity;
+
+public class Circle : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+
+        var linePoints = new List<Vector3>(100);
+        VectorLine myLine = new VectorLine("Line", linePoints, 8.0f, LineType.Continuous, Joins.Fill);
+        myLine.SetColor(Color.yellow);
+        myLine.MakeCircle(new Vector3 (0,0,0), new Vector3(1, 0, 0), 0.5f,50);
+        myLine.drawTransform = transform;
+        myLine.Draw3DAuto();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
