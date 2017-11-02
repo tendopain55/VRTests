9955d01002e48b9b46388f34e2d7b624db559d4b
Unknown <wht5500@gmail.com>
2017-11-02 14:40:09 -0700

    Vectrosity Install

---
+++ Assets/UpdatingText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NewtonVR
+{
+    public class UpdatingText : MonoBehaviour
+    {
+
+        Text sliderinput;
+        public NVRSlider Slider;
+        void Start()
+        {
+            sliderinput = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+            string output = Slider.CurrentValue.ToString("0.00");
+            sliderinput.text = output;
+
+        }
+    }
+}
---
+++ Assets/exampleCircle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+public class exampleCircle : MonoBehaviour
+{
+
+    public float RotateSpeed = 5f;
+    public float Radius = 1.0f;
+
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = transform.localPosition;
+    }
+
+    private void Update()
+    {
+
+        _angle += RotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        transform.position = _centre + offset;
+    }
+
+
+
+}
