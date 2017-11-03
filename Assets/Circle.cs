b005a102af1e03453e63934a5e313a78a875906c
Unknown <wht5500@gmail.com>
2017-11-02 17:20:08 -0700

    Real Circle Tied

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vectrosity;
-
 public class Circle : MonoBehaviour {
-    
+    public int segments=50;
+    public float radius=0.5f;
+    public float thickness=5.0f;
 	// Use this for initialization
 	void Start () {
-
-        var linePoints = new List<Vector3>(100);
-        VectorLine myLine = new VectorLine("Line", linePoints, 8.0f, LineType.Continuous, Joins.Fill);
-        myLine.SetColor(Color.yellow);
-        myLine.MakeCircle(new Vector3 (0,0,0), new Vector3(1, 0, 0), 0.5f,50);
-        myLine.drawTransform = transform;
+        var linePoints = new List<Vector3>(segments*2);
+        var myLine = new VectorLine("Line", linePoints, thickness, LineType.Continuous, Joins.Weld);
+        myLine.color = Color.red;
+        myLine.MakeCircle(new Vector3(0,0,0), new Vector3(1,0,0), radius, segments);
         myLine.Draw3DAuto();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-    }
+		
+	}
 }
