2679373eccb6b4531100783ee06b6ca9c18a77c8
Unknown <wht5500@gmail.com>
2017-11-02 18:05:50 -0700

    11/2/2017 6:05

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -6,12 +6,14 @@ public class Circle : MonoBehaviour {
     public int segments=50;
     public float radius=0.5f;
     public float thickness=5.0f;
+    public float offset = 0.0f;
 	// Use this for initialization
 	void Start () {
         var linePoints = new List<Vector3>(segments*2);
         var myLine = new VectorLine("Line", linePoints, thickness, LineType.Discrete, Joins.Weld);
+        myLine.drawTransform = transform;
         myLine.color = Color.red;
-        myLine.MakeCircle(new Vector3(0,0,0), new Vector3(1,0,0), radius, segments);
+        myLine.MakeCircle(new Vector3(offset,0,0), new Vector3(1,0,0), radius, segments);
         myLine.Draw3DAuto();
 
     }
