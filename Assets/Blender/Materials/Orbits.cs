6b4e4c9716bf56b36bc1fff7a8416575eb6f3f27
Unknown <wht5500@gmail.com>
2018-03-14 14:56:21 -0700

    3/14 Orbits
    Starting to make failing orbit code

---
+++ Assets/Blender/Materials/Orbits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbits : MonoBehaviour {
+    public Rigidbody rb;
+    public Rigidbody center;
+    public bool clockwise;
+	
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        double velocity = 0.0;
+        velocity = System.Math.Sqrt(System.Math.Pow(6.674, -11.0) * center.mass / (center.position - this.transform.position).magnitude);
+        if (clockwise) {
+            rb.velocity = new Vector3(0, 0, (float) velocity);
+        } else
+        {
+            rb.velocity = new Vector3(0, 0, (float)velocity);
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        double force = System.Math.Pow(6.674, -11.0) * rb.mass * center.mass / ((center.position - this.transform.position).magnitude * (center.position - this.transform.position).magnitude)*100000;
+        rb.AddForce((center.position - this.transform.position).normalized*(float)force);
+	}
+}
