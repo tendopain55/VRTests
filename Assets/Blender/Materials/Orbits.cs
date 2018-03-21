a502fcd2f64e8438475b32025dbd89902f4f4b1b
Unknown <wht5500@gmail.com>
2018-03-21 14:55:41 -0700

    threetwoone
    orbits should be functional

--- Assets/Blender/Materials/Orbits.cs
+++ Assets/Blender/Materials/Orbits.cs
@@ -1,27 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NewtonVR;
 
 public class Orbits : MonoBehaviour {
     public Rigidbody rb;
     public Rigidbody center;
     public bool clockwise;
-	
+    private NVRInteractableItem item;
+	private Vector3 lastPos;
 	void Start () {
         rb = GetComponent<Rigidbody>();
-        double velocity = 0.0;
-        velocity = System.Math.Sqrt(System.Math.Pow(6.674, -11.0) * center.mass / (center.position - this.transform.position).magnitude);
-        if (clockwise) {
-            rb.velocity = new Vector3(0, 0, (float) velocity);
-        } else
-        {
-            rb.velocity = new Vector3(0, 0, (float)velocity);
-        }
+        item = GetComponent<NVRInteractableItem>();
+        lastPos = center.transform.position;
+        float initV = Mathf.Sqrt(center.mass / (center.position - this.transform.position).magnitude);
+        InitialVelocity();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        double force = System.Math.Pow(6.674, -11.0) * rb.mass * center.mass / ((center.position - this.transform.position).magnitude * (center.position - this.transform.position).magnitude)*100000;
-        rb.AddForce((center.position - this.transform.position).normalized*(float)force);
+        item.OnEndInteraction.AddListener(InitialVelocity);
+        this.transform.position += center.transform.position - lastPos;
+        float r = Vector3.Distance(center.transform.position, this.transform.position);
+        float forceVal = -(center.mass * this.rb.mass) / (r * r);
+        Vector3 force = (this.transform.position - center.transform.position).normalized * forceVal;
+        rb.AddForce(force);
+        lastPos = center.transform.position;
 	}
+    void InitialVelocity()
+    {
+        float initV = Mathf.Sqrt(center.mass / (center.position - this.transform.position).magnitude);
+        if (clockwise)
+        {
+            rb.velocity = Vector3.Cross((center.position - this.transform.position), new Vector3(0, 1, 0)).normalized * initV;
+
+        }
+        else
+        {
+            rb.velocity = Vector3.Cross((center.position - this.transform.position), new Vector3(0, 1, 0)).normalized * -initV;
+        }
+    }
 }
