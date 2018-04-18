cddd3946c40d37a35c55be19ebad894df0c93354
Unknown <wht5500@gmail.com>
2018-04-18 14:10:39 -0700

    updating orbits
    making orbits work with multiple scripts attatched

--- Assets/Blender/Materials/Orbits.cs
+++ Assets/Blender/Materials/Orbits.cs
@@ -7,37 +7,70 @@ public class Orbits : MonoBehaviour {
     public Rigidbody rb;
     public Rigidbody center;
     public bool clockwise;
+    private NVRInteractableItem centerItem;
     private NVRInteractableItem item;
 	private Vector3 lastPos;
+    public Orbits innerOrbit;
+    private bool update = true;
 	void Start () {
         rb = GetComponent<Rigidbody>();
         item = GetComponent<NVRInteractableItem>();
-        lastPos = center.transform.position;
-        float initV = Mathf.Sqrt(center.mass / (center.position - this.transform.position).magnitude);
+        centerItem = center.GetComponent<NVRInteractableItem>();
+        lastPos = center.position;
+        float initV = Mathf.Sqrt(center.mass / (center.position - this.rb.position).magnitude);
         InitialVelocity();
-	}
+        item.OnEndInteraction.AddListener(resetV);
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        item.OnEndInteraction.AddListener(InitialVelocity);
-        this.transform.position += center.transform.position - lastPos;
-        float r = Vector3.Distance(center.transform.position, this.transform.position);
-        float forceVal = -(center.mass * this.rb.mass) / (r * r);
-        Vector3 force = (this.transform.position - center.transform.position).normalized * forceVal;
-        rb.AddForce(force);
-        lastPos = center.transform.position;
+        if (centerItem != null)
+        {
+            if (centerItem.IsAttached)
+            {
+                if (innerOrbit != null)
+                {
+                    innerOrbit.update = false;
+                }
+                this.rb.position += center.position - lastPos;
+                lastPos = center.position;
+                pauseV();
+                return;
+            }
+        }
+        if (update)
+        {
+            float r = Vector3.Distance(center.position, this.rb.position);
+            float forceVal = -(center.mass * this.rb.mass) / (r * r);
+            Vector3 force = (this.rb.position - center.position).normalized * forceVal;
+            rb.AddForce(force);
+        }
 	}
     void InitialVelocity()
     {
-        float initV = Mathf.Sqrt(center.mass / (center.position - this.transform.position).magnitude);
+        if(innerOrbit != null){
+            innerOrbit.update = true;
+        }
+        float initV = Mathf.Sqrt(center.mass / (center.position - this.rb.position).magnitude);
         if (clockwise)
         {
-            rb.velocity = Vector3.Cross((center.position - this.transform.position), new Vector3(0, 1, 0)).normalized * initV;
+            rb.velocity += Vector3.Cross((center.position - this.rb.position), new Vector3(0, 1, 0)).normalized * initV;
 
         }
         else
         {
-            rb.velocity = Vector3.Cross((center.position - this.transform.position), new Vector3(0, 1, 0)).normalized * -initV;
+            rb.velocity += Vector3.Cross((center.position - this.rb.position), new Vector3(0, 1, 0)).normalized * -initV;
         }
+    }
+
+    void resetV()
+    {
+        this.rb.velocity = new Vector3(0,0,0);
+        InitialVelocity();
+    }
+
+    void pauseV()
+    {
+        this.rb.velocity = new Vector3(0, 0, 0);
     }
 }
