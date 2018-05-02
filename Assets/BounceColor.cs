851a0d20bf3dd3ef08a55d998646c574484693b6
Unknown <wht5500@gmail.com>
2018-05-02 13:49:22 -0700

    5/2 Bounciness
    made new collision elasticity demo

---
+++ Assets/BounceColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceColor : MonoBehaviour {
+    Material m;
+    PhysicMaterial p;
+	// Use this for initialization
+	void Start () {
+        m = GetComponent<Material>();
+        Color c = GetComponent<Color>();
+        c.a *= p.bounciness;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
