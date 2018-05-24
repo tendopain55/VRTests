4c3a6cfb7acfd8abfe71b05e71d5b2095eafb679
Unknown <wht5500@gmail.com>
2018-05-24 16:36:22 -0700

    Many Changes and Touchups
    Mostly skybox and final scene related, touching up environments as well

---
+++ Assets/StartSlow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSlow : MonoBehaviour {
+    public float timeScale  = 0.5f;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = this.timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
